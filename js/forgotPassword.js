894750595338c6c8fb6b5ed46873b5554332e4a8
Huan <tonghohuan2012@gmail.com>
2026-05-23 15:28:11 +0700

    update

--- js/forgotPassword.js
+++ js/forgotPassword.js
@@ -4,18 +4,16 @@ form.addEventListener("submit", (event) => {
     // Ngặn chặn hành động load mặc định của sever
     event.preventDefault();
 
-    let username = document.getElementById("username").value.trim();
     let email = document.getElementById("email").value.trim();
     let password = document.getElementById("password").value.trim();
+    let confirmPassword = document.getElementById("confirmPassword").value.trim();
 
     // Regex kiểm tra kí tự 
     let lowerCaseLetter = /[a-z]/g;
     let upperCaseLetter = /[A-Z]/g;
     let numbers = /[0-9]/g;
 
-    if (username.length < 6) {
-        alert("Username must be at least 6 characters");
-    } else if (password.length < 8) {
+    if (password.length < 8) {
         alert("Password must be at least 8 characters");
         // Kiểm tra password có kí tự viết thường không
     } else if (!password.match(lowerCaseLetter)) {
@@ -25,33 +23,22 @@ form.addEventListener("submit", (event) => {
         alert("Password must contain an uppercase letter");
     } else if (!password.match(numbers)) {
         alert("Password must contain a number or special character");
+    } else if (password !== confirmPassword) {
+        alert("Password and confirm password do not match");
     } else {
-        if (localStorage.getItem("users")) {
-            // Chuyển từ json sang object
-            let users = JSON.parse(localStorage.getItem("users"));
+        let users = JSON.parse(localStorage.getItem("users"));
+        // Find trong javascript sẽ trả về giá trị đúng với điều kiện
+        let user = users.find((account) => {
+            return account.email === email;
+        });
 
-            let newAccount = {
-                email,
-                password,
-                username,
-            };
-
-            users.push(newAccount);
-
+        if (user) {
+            user.password = password;
             localStorage.setItem("users", JSON.stringify(users));
+            alert("Reset password successfully");
+            location.href = "./login.html";
         } else {
-            let mangUser = [];
-            let newAccount = {
-                email,
-                password,
-                username,
-            };
-            mangUser.push(newAccount);
-            // Chuyển dữ liệu từ object thành json 
-            let userJson = JSON.stringify(mangUser);
-            localStorage.setItem("users", userJson);
+            alert("Email email is incorrect");
         }
-        alert("User created successfully, please login");
-        location.href = "./login.html";
     }
 });
